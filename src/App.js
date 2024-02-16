3595db3582b64f88a02256d0c0a62ce8dac1b727
DanielKorban <74277609+daniel99korban@users.noreply.github.com>
2024-02-16 04:04:59 +0000

    new component has been created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import TipoPokemons from './components/TipoPokemos'
 
 function App() {
   return (
@@ -18,6 +19,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <div>
+        <TipoPokemons/>
+      </div>
     </div>
   );
 }
