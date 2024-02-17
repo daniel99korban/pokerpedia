709f92324b0b73125dfdb3eaea52a7e7a5307352
DanielKorban <74277609+daniel99korban@users.noreply.github.com>
2024-02-17 02:33:01 +0000

    refatoração

--- src/components/TipoPokemos.jsx
+++ src/components/TipoPokemos.jsx
@@ -1,53 +1,66 @@
 import { useState, useEffect } from "react";
+import ListaPokemons from "./ListaPokemons";
 
 function TipoPokemons(){
 
     let url = "https://pokeapi.co/api/v2/type/";
     
     let [categorias, setCategorias] = useState();
-    let [descricao, setDescricao] = useState();
+    let [pokemons, setPokemons] = useState();
+    let [tipoPokemon, setTipoSelecionado] = useState('normal');
+
 
     useEffect(() => {
         let obterCategorias = async () => {
-            let response = await fetch(url);
-            let responseJSON = await response.json();
+            let tipoPokemonResponse = await fetch(url);
+            let responseJSON = await tipoPokemonResponse.json();
+
+
             let index = 0;
 
             let listaCategorias = responseJSON.results.map(categoria => {
                 index += 1;
                 return(
-                    <a class="btn btn-primary" data-bs-toggle="collapse" href={`#collapse-${index}`} role="button" aria-expanded="false" aria-controls="collapseExample">
-                        {categoria.name}
-                    </a>
+                    <>
+                        {
+                            categoria.name==tipoPokemon?
+                            <button class="btn btn-primary" data-bs-toggle="collapse" href={`#collapse-${index}`} role="button" aria-expanded="false" aria-controls={`collapse-${index}`}>
+                                {categoria.name}
+                            </button>
+                            :
+                            <button onClick={()=>{setTipoSelecionado(categoria.name)}} class="btn btn-secondary" data-bs-toggle="collapse" href={`#collapse-${index}`} role="button" aria-expanded="false" aria-controls={`collapse-${index}`}>
+                                {categoria.name}
+                            </button>
+                        }
+                    </>
                 )
             });
 
             index = 0;
 
-            let listaDescricao =  responseJSON.results.map(descricao =>{
+            let listaPokemons =  responseJSON.results.map(tipoPokemon => {
                 index += 1;
                 return(
                     <div class="collapse" id={`collapse-${index}`}>
-                        <div class="card card-body">{descricao.url}</div>
+                        <div class="card card-body">
+                            <ListaPokemons url={tipoPokemon.url} name={tipoPokemon.name}/>
+                        </div>
                     </div>
                 )
             });
 
-
             setCategorias(listaCategorias);
-            setDescricao(listaDescricao);
+            setPokemons(listaPokemons);
         }
         obterCategorias();
-    }, [])
-    
-    console.log(categorias);
+    }, [tipoPokemon])
 
     return (
         <>
             <p class="d-inline-flex gap-1">
                 {categorias}
             </p>
-            {descricao}
+            {pokemons}
         </>
     );
 
