76d5299ababe868317b114a7facb1c0fc6630187
DanielKorban <74277609+daniel99korban@users.noreply.github.com>
2024-02-17 18:52:55 +0000

    refactoring

--- src/components/ListaPokemons.jsx
+++ src/components/ListaPokemons.jsx
@@ -1,53 +1,51 @@
 import { useEffect, useState } from "react";
 
 export default function ListaPokemons({name, url}){
-
     let [pokeDados, setPokeDados] = useState();
-    let [imgPokemon, setImgPokemon] = useState();
-    
+       
     useEffect(()=>{
-
+        
         let obterPokemons = async () => {
             let pokemonsResponse = await fetch(url);
             let pokemonsResponseJSON = await pokemonsResponse.json();
             
+            
+            const pokemons = await Promise.all( 
+                pokemonsResponseJSON.pokemon.map( async (dados) => {
+                    let img = obterImgPokemon(dados.pokemon.url);
+                    return {
+                        name : dados.pokemon.name,
+                        img : img
+                    }
+                })
+            )
 
-            let pokemons = pokemonsResponseJSON.pokemon.map((dados) => {
-
-                let recuperarDescricaoPokemon =  () => {
-                    // let pokeInfResponse = await fetch(dados.pokemon.url);
-                    // let pokeInfResponseJson = await pokeInfResponse.json();
-                    // setImgPokemon(pokeInfResponseJson.sprites.back_default);
-                }
-                
-                console.log(dados.pokemon.url);
-                // recuperarDescricaoPokemon();
-
-                return(
-                    <div className="col">
-                        <div className="card h-100">
-                            <img src={imgPokemon} className="card-img-top" alt="..."/>
-                            <div className="card-body">
-                                <h5 className="card-title"><strong>Nome: </strong>{dados.pokemon.name}</h5>
-                                <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            </div>
-                        </div>
-                    </div>
-                )
-            })
-
-            let listaPokemons = <ul>{pokemons}</ul>
-            setPokeDados(listaPokemons);
+            setPokeDados(pokemons);
         }
 
         obterPokemons();
-    },[])
+
+    },[url])
+
+    let obterImgPokemon = async (url) => {
+        let imgResponse = await fetch(url);
+        let imgResponseJson = await imgResponse.json();
+        return imgResponseJson.sprites.back_default;
+    }
 
     return(
         <>
             <p>Pokemons tipo <strong>{name}</strong></p>
             <div class="row row-cols-1 row-cols-md-3 g-4">
-                {pokeDados}
+                <div className="col">
+                    <div className="card h-100">
+                        <img src={imgPokemon} className="card-img-top" alt="..."/>
+                        <div className="card-body">
+                            <h5 className="card-title"><strong>Nome: </strong>{dados.pokemon.name}</h5>
+                            <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     )
